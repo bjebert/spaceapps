607f9b86abd39f084481f4d81c3542aabf27e800
David <dave_stubb@hotmail.com>
2017-04-29 14:05:48 +1000

    added moon mesh

---
+++ Origami/Assets/Cartesian.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartesianCoordinate
+{
+    readonly private double x;
+    readonly private double y;
+    readonly private double z;
+
+    public CartesianCoordinate()
+    {
+
+    }
+
+}
