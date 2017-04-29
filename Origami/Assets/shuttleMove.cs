9af05ab6b185f724fcd7944581faf495cf0e9884
TimY <timy.cross@gmail.com>
2017-04-29 15:39:28 +1000

    Moving Shuttle

--- Origami/Assets/shuttleMove.cs
+++ Origami/Assets/shuttleMove.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class shuttleMove : MonoBehaviour {
 
-    public const float SPEED = 2.0f; // Speed weighting
-    public Transform target;
+    public const float SPEED = 0.1f; // Speed weighting
+    public Vector3 target;
     
     void Start()
     {
-        target = this.gameObject.GetComponentInChildren<MeshRenderer>().transform;
-        target.position += Vector3.up * 10.0f;
+        target = transform.position;
+        target += Vector3.up * 100000.0f;
 
 
     }
 
     void Update()
     {
-        
+
         float step = SPEED * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
     }
 
 }
